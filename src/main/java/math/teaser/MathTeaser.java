20f57044377096f29d9e3b5eaa75f661d818b894
Mikhail Deviatov <midnighterdev@gmail.com>
2023-12-17 11:29:27 +0100

    add math with time

--- src/main/java/math/teaser/MathTeaser.java
+++ src/main/java/math/teaser/MathTeaser.java
@@ -3,6 +3,7 @@ package math.teaser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalTime;
 import java.util.Random;
 
 public class MathTeaser {
@@ -21,12 +22,13 @@ public class MathTeaser {
             int number2 = 0;
             int operation = 0;
             int result = 0;
+            LocalTime localTime = LocalTime.now();
             boolean isConvertTo = true;
             do {
                 if (showExpression) {
                     number1 = generateNumber(random, 139, 10);
                     number2 = generateNumber(random, 139, 10 );
-                    operation = random.nextInt(5);
+                    operation = random.nextInt(2);
                     switch (operation) {
                         case 0:
                             result = addition(number1, number2);
@@ -37,14 +39,19 @@ public class MathTeaser {
                             System.out.print(number1 + " - " + number2 + " = ");
                             break;
                         case 2:
-                            number1 = generateNumber(random, 18, 6 );
-                            number2 = generateNumber(random, 18, 4 );
+                            number1 = generateNumber(random, 16, 2);
+                            number2 = generateNumber(random, 14, 7);
                             result = multiply(number1, number2);
                             System.out.print(number1 + " * " + number2 + " = ");
                             break;
                         case 3:
-                            number1 = generateNumber(random, 90, 10 );
-                            number2 = generateNumber(random, 8, 2);
+                            number1 = generateNumber(random, 80, 11 );
+                            number2 = generateNumber(random, 12, 2);
+                            if (Math.abs(number1) < Math.abs(number2)) {
+                                int tmp = number1;
+                                number1 = number2;
+                                number2 = tmp;
+                            }
                             final int modulus = modulus(number1, number2);
                             if (modulus != 0) {
                                 result = division(number1 - modulus, number2);
@@ -54,14 +61,23 @@ public class MathTeaser {
                             System.out.print(number1 + " / " + number2 + " = ");
                             break;
                         case 4:
-                            number1 = convert(random, 7, 9);
+                            number1 = Math.abs(generateNumber(random, 600,  2));
+                            number2 = Math.abs(generateNumber(random, 600,  2));
+                            localTime = LocalTime.ofSecondOfDay(number1).plusSeconds(number2);
+                            System.out.print(LocalTime.ofSecondOfDay(number1) + " + " + LocalTime.ofSecondOfDay(number2) + " = ");
+                            break;
+                        case 5:
+                            number1 = convert(random, 100, 10);
                             isConvertTo = random.nextBoolean();
                             if (isConvertTo) {
-                                System.out.print(number1 + " 0x" + " = ");
+                                System.out.print(number1 + " to 0x" + " = ");
                             } else {
                                 System.out.print("0x" + Integer.toHexString(number1).toUpperCase() + " = ");
                             }
                             break;
+                        case 6:
+                            // devide number to consist of multiple numbers, ad comparison. (for example one number
+                            // has to be less than, other more than)
                     }
 
                     showExpression = false;
@@ -85,8 +101,11 @@ public class MathTeaser {
                             }
                             break;
                         case 4:
+                            System.out.print(LocalTime.ofSecondOfDay(number1) + " + " + LocalTime.ofSecondOfDay(number2) + " = " + localTime + "\n");
+                            break;
+                        case 5:
                             if (isConvertTo) {
-                                System.out.print(number1 + " 0x" + " = 0x" + Integer.toHexString(number1).toUpperCase() + "\n");
+                                System.out.print(number1 + " to 0x" + " = 0x" + Integer.toHexString(number1).toUpperCase() + "\n");
                             } else {
                                 System.out.print("0x" + Integer.toHexString(number1).toUpperCase() + " = " + number1 + "\n");
                             }
