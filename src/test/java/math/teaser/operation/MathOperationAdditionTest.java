54caab2fa712f2c9359563899252e4fba8bb7452
Mikhail Deviatov <midnighterdev@gmail.com>
2025-01-03 21:06:37 +0100

    update test cases for addition and multiplication

--- src/test/java/math/teaser/operation/MathOperationAdditionTest.java
+++ src/test/java/math/teaser/operation/MathOperationAdditionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static math.teaser.operation.MathOperationType.ADDITION;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -12,25 +13,41 @@ public class MathOperationAdditionTest {
 
     @Test
     void addTwoPositiveNumbersTest() {
-        int sum = Integer.parseInt(ADDITION.apply(4, 2));
-        assertEquals(6, sum);
+        int sum1 = Integer.parseInt(ADDITION.apply(4, 2));
+        int sum2 = Integer.parseInt(ADDITION.apply(2, 4));
+        assertAll(
+                () -> assertEquals(6, sum1),
+                () -> assertEquals(6, sum2)
+        );
     }
 
     @Test
     void addPositiveAndNegativeNumbersTest() {
-        int sum = Integer.parseInt(ADDITION.apply(4, -2));
-        assertEquals(2, sum);
+        int sum1 = Integer.parseInt(ADDITION.apply(4, -2));
+        int sum2 = Integer.parseInt(ADDITION.apply(-2, 4));
+        assertAll(
+                () -> assertEquals(2, sum1),
+                () -> assertEquals(2, sum2)
+        );
     }
 
     @Test
     void addNegativeAndPositiveNumbersTest() {
-        int sum = Integer.parseInt(ADDITION.apply(-4, 2));
-        assertEquals(-2, sum);
+        int sum1 = Integer.parseInt(ADDITION.apply(-4, 2));
+        int sum2 = Integer.parseInt(ADDITION.apply(2, -4));
+        assertAll(
+                () -> assertEquals(-2, sum1),
+                () -> assertEquals(-2, sum2)
+        );
     }
 
     @Test
     void addTwoNegativeNumbersTest() {
-        int sum = Integer.parseInt(ADDITION.apply(-4, -2));
-        assertEquals(-6, sum);
+        int sum1 = Integer.parseInt(ADDITION.apply(-4, -2));
+        int sum2 = Integer.parseInt(ADDITION.apply(-2, -4));
+        assertAll(
+                () -> assertEquals(-6, sum1),
+                () -> assertEquals(-6, sum2)
+        );
     }
 }
--- src/test/java/math/teaser/operation/MathOperationMultiplicationTest.java
+++ src/test/java/math/teaser/operation/MathOperationMultiplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static math.teaser.operation.MathOperationType.MULTIPLICATION;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -12,25 +13,41 @@ public class MathOperationMultiplicationTest {
 
     @Test
     void multiplyTwoPositiveNumbersTest() {
-        int product = Integer.parseInt(MULTIPLICATION.apply(4, 2));
-        assertEquals(8, product);
+        int product1 = Integer.parseInt(MULTIPLICATION.apply(4, 2));
+        int product2 = Integer.parseInt(MULTIPLICATION.apply(2, 4));
+        assertAll(
+                () -> assertEquals(8, product1),
+                () -> assertEquals(8, product2)
+        );
     }
 
     @Test
     void multiplyPositiveAndNegativeNumbersTest() {
-        int product = Integer.parseInt(MULTIPLICATION.apply(4, -2));
-        assertEquals(-8, product);
+        int product1 = Integer.parseInt(MULTIPLICATION.apply(4, -2));
+        int product2 = Integer.parseInt(MULTIPLICATION.apply(-2, 4));
+        assertAll(
+                () -> assertEquals(-8, product1),
+                () -> assertEquals(-8, product2)
+        );
     }
 
     @Test
     void multiplyNegativeAndPositiveNumbersTest() {
-        int product = Integer.parseInt(MULTIPLICATION.apply(-4, 2));
-        assertEquals(-8, product);
+        int product1 = Integer.parseInt(MULTIPLICATION.apply(-4, 2));
+        int product2 = Integer.parseInt(MULTIPLICATION.apply(2, -4));
+        assertAll(
+                () -> assertEquals(-8, product1),
+                () -> assertEquals(-8, product2)
+        );
     }
 
     @Test
     void multiplyTwoNegativeNumbersTest() {
-        int product = Integer.parseInt(MULTIPLICATION.apply(-4, -2));
-        assertEquals(8, product);
+        int product1 = Integer.parseInt(MULTIPLICATION.apply(-4, -2));
+        int product2 = Integer.parseInt(MULTIPLICATION.apply(-2, -4));
+        assertAll(
+                () -> assertEquals(8, product1),
+                () -> assertEquals(8, product2)
+        );
     }
 }
