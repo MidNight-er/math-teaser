66bb994fe2db68a05d1cb2b520432b2afc79525d
Mikhail Deviatov <midnighterdev@gmail.com>
2025-01-04 08:24:05 +0100

    update subtraction test case

--- src/test/java/math/teaser/operation/MathOperationSubtractionTest.java
+++ src/test/java/math/teaser/operation/MathOperationSubtractionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static math.teaser.operation.MathOperationType.SUBTRACTION;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -12,26 +13,33 @@ public class MathOperationSubtractionTest {
 
     @Test
     void subtractTwoPositiveNumbersTest() {
-        int difference = Integer.parseInt(SUBTRACTION.apply(4, 2));
-        assertEquals(2, difference);
+        assertAll(
+                () -> assertEquals(2, Integer.parseInt(SUBTRACTION.apply(4, 2))),
+                () -> assertEquals(-2, Integer.parseInt(SUBTRACTION.apply(2, 4)))
+        );
     }
 
     @Test
     void subtractPositiveAndNegativeNumbersTest() {
-        int difference = Integer.parseInt(SUBTRACTION.apply(4, -2));
-        assertEquals(6, difference);
+        assertAll(
+                () -> assertEquals(6, Integer.parseInt(SUBTRACTION.apply(4, -2))),
+                () -> assertEquals(-6, Integer.parseInt(SUBTRACTION.apply(-2, 4)))
+        );
     }
 
     @Test
     void subtractNegativeAndPositiveNumbersTest() {
-        int difference = Integer.parseInt(SUBTRACTION.apply(-4, 2));
-        assertEquals(-6, difference);
+        assertAll(
+                () -> assertEquals(-6, Integer.parseInt(SUBTRACTION.apply(-4, 2))),
+                () -> assertEquals(6, Integer.parseInt(SUBTRACTION.apply(2, -4)))
+        );
     }
 
     @Test
     void subtractTwoNegativeNumbersTest() {
-        int difference = Integer.parseInt(SUBTRACTION.apply(-4, -2));
-        assertEquals(-2, difference);
+        assertAll(
+                () -> assertEquals(-2, Integer.parseInt(SUBTRACTION.apply(-4, -2))),
+                () -> assertEquals(2, Integer.parseInt(SUBTRACTION.apply(-2, -4)))
+        );
     }
-
 }
